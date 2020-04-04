5d8d7125a65c7ce7d623a13deb01a63f5c824b42
Movle <richard.xiu.520@gmail.com>
2020-04-04 16:42:01 +0800

    第一次提交

---
+++ src/main/java/Test/HelloWorld.java
@@ -0,0 +1,18 @@
+package Test;
+
+/**
+ * @ClassName HelloWorld
+ * @MethodDesc: TODO HelloWorld功能介绍
+ * @Author Movle
+ * @Date 4/4/20 4:25 下午
+ * @Version 1.0
+ * @Email movle_xjk@foxmail.com
+ **/
+
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
